c31122d0624087f1a8a4be4f94240118911ca2d4
alibeknurgaliyev <a.nurgaliyev@mycar.kz>
2023-09-29 15:08:21 +0600

    добавил HelloWorld

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        helloWorld()
+    }
+
+    private fun helloWorld(){
+        Log.d("TAG","Hello world")
     }
 }
